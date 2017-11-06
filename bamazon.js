6432beb6fa155da09704f40570c825c35b1edba6
tiffanyyyim <yim.tiffany@gmail.com>
2017-11-06 21:55:37 +0000

    working inquirer and connection to sql database

--- bamazon.js
+++ bamazon.js
@@ -9,27 +9,31 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-//connection.connect(function(err){
-//    if (err) throw err;
-//    console.log("connected as id" + connection.threadID);
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
 //    listBamazonItems();
-//});
-//
+    ask();
+});
+
 //function listBamazonItems(){
-//    connection.query("Select * from products", function(err, res){
+//    connection.query("Select item_id, product_name, price from products", function(err, res){
 //        if (err) throw err; 
 //        console.log(res);
-//        connection.end();
-//    });
 //    
+//    });
+//}
+    
     
 //Prompt the user, what would you like to buy? 
+function ask(){
     inquirer.prompt([
     {
         type: "input",
         name: "itemToBuy",
-        message: "What items do you want to buy?"
-    },
+        message: "What item_id do you want to buy?"
+    }
+        ,
     {
 //Prompt the user, how many would you like to buy? 
         type: "input",
@@ -37,40 +41,44 @@ var connection = mysql.createConnection({
         message: "How many do you want to buy?"
     }
     ])
-    .then(function(answers){
+    .then(function(answer){
         //sets the users asnwer to a variable
-        var userItem = answers.itemToBuy; 
-        //console log to check above line is working
-        console.log(userItem);
+        var userItem = answer.itemToBuy; 
         //query varible
-        var query = "Select stock_quantity from products where item_id ?";
+        var query = "Select stock_quantity from products where ?";
         //running the query against the database with the query and answer from above 
-        connection.query(query, {id: answers.itemToBuy}, function(err, res){
-            console.log(res);
-        });
+        connection.query(query, {item_id: answer.itemToBuy}, function(err, res){
+        if (err) throw err;
+        console.log(res);
+        });    
+        
+     });
+}  
+
         
         //sets the users second answer to a variable
-        var userQuantity = answers.quantity; 
-        //console log to check above line is working 
-        console.log(userQuantity);
-        if (userQuantity <= results){
-            console.log("you bought it");
-        } else 
-        {
-            console.log("there is not enough stock");
-        }
-        
-    });
+//        var userQuantity = answers.quantity; 
+//        //console log to check above line is working 
+//        console.log(userQuantity);
+//        if (userQuantity <= results){
+//            console.log("you bought it");
+//        } else 
+//        {
+//            console.log("there is not enough stock");
+//        }
+//        
+//    });
+//}
 
 //function compareUserQunntityToDB() {
 //    var query = "Select stock_quantity from products where item_id ?";
 //    connection.query(query, {id: answers.itemToBuy}, function(err, res){
 //        console.log(res);
 //    });
-//}
+////}
 //run the SQL query: select stock_quantity from products where item_id = 1
 //compare the SQL query result to the variable from user's second question answer 
     //if user's answer <= store quantity, 
         //Your total = quanity * price from database 
         //database quantiy decreases by current quantity minus user's input 
-    //else tell the user "insufficient quantity!" 
+    //else tell the user "insufficient quantity!"
